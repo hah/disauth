50e2c4a1465f410c17b968b7f257432fa3d0cd18
gyula <isnt@gyula.cool>
2020-02-12 12:05:40 +0100

    revoke token path added
    small refactoring as well

--- src/disauth/client.cr
+++ src/disauth/client.cr
@@ -1,6 +1,7 @@
 require "json"
 require "http/client"
 require "openssl/ssl/context"
+require "base64"
 
 module Discord
 
@@ -10,7 +11,7 @@ module Discord
     API_BASE    = "https://discordapp.com/api"
 
     def request(method : String, path : String, headers : HTTP::Headers, body : String?)
-      headers["User-Agent"] = USER_AGENT
+      headers.add("user-agent", USER_AGENT)
       response = HTTP::Client.exec(method: method, url: API_BASE + path, headers: headers, body: body, tls: SSL_CONTEXT)
       return {"status" => response.status_code, "data" => JSON.parse(response.body)}.to_json
     end
@@ -26,16 +27,27 @@ module Discord
         form.add "redirect_uri", @redirect_uri
         form.add "scope", "identify email"
       end
-    
       headers = HTTP::Headers.new
       headers.add("content-type", "application/x-www-form-urlencoded")
       return request("POST", "/oauth2/token", headers, params)
-      
     end
 
-    def getUser(token)
+    def revokeToken(token : String)
+      params = HTTP::Params.build do |form|
+        form.add "token", token
+      end
       headers = HTTP::Headers.new
-      headers["Authorization"] = "Bearer #{token}"
+      # Base64.encode inserts a new line after every 60 characters
+      # gsub to replace all occurances to get a one-liner for auth
+      authkey = Base64.encode("#{@client_id}:#{@client_secret}").gsub("\n","") 
+      headers.add("authorization", "Basic #{authkey}")
+      headers.add("content-type", "application/x-www-form-urlencoded")
+      return request("POST", "/oauth2/token/revoke", headers, params)
+    end
+
+    def getUser(token : String)
+      headers = HTTP::Headers.new
+      headers.add("authorization", "Bearer #{token}")
       return request("GET","/users/@me", headers, nil)
     end
 
